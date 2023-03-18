e08ac10abde096b7924ffb9aa5ba762739e5d8f0
Gabriel4420 <gabriel_rodrigues_perez@hotmail.com>
2023-03-17 23:36:04 -0300

    recente changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
-
+import { Component } from '@angular/core'
+import { ContactsService } from './services/contacts.service'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { ToastrService } from 'ngx-toastr/public_api'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'portifolio-web';
+  formulario!: FormGroup
+  public messageError!: string
+  constructor(
+    private contactService: ContactsService,
+    private formBuilder: FormBuilder,
+    private toastrService: ToastrService,
+  ) {}
+
+  ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      subject: ['', [Validators.required]],
+      message: ['', [Validators.required]],
+    })
+  }
+
+  sendMessage() {
+    this.contactService
+      .addContact({
+        Name: this.formulario.get('name')?.value,
+        Email: this.formulario.get('email')?.value,
+        Subject: this.formulario.get('subject')?.value,
+        Message: this.formulario.get('message')?.value,
+      })
+      .subscribe({
+        next: () => {
+          this.formulario.setValue({
+            name: '',
+            email: '',
+            subject: '',
+            message: '',
+          })
+        },
+        error: (e) => (this.messageError = e),
+      })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
-
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { HttpClientModule } from '@angular/common/http'
+import { AppComponent } from './app.component'
+import { ReactiveFormsModule } from '@angular/forms'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { ToastrModule } from 'ngx-toastr'
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
